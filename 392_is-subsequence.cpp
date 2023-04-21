bdc03c682ef52397d69923294c4ab9e483915295
For Kirie <1048776386@qq.com>
2023-04-21 10:47:22 +0800

    LeetCode 392. 判断子序列(DP)

--- 392_is-subsequence.cpp
+++ 392_is-subsequence.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 class Solution
 {
@@ -27,8 +28,46 @@ public:
     }
 };
 
+class Solution2
+{
+public:
+    bool isSubsequence(std::string s, std::string t)
+    {
+        // dp[i][j]:以s[i]为结尾的子串与以t[j]为结尾的子串的最长公共子序列的长度
+        std::vector<std::vector<int>> dp(s.size() + 1, std::vector<int>(t.size() + 1, 0));
+
+        for (int i = 1; i <= s.size(); ++i)
+        {
+            for (int j = 1; j <= t.size(); ++j)
+            {
+                if (s[i - 1] == t[j - 1])
+                {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                else
+                {
+                    dp[i][j] = dp[i][j - 1], dp[i - 1][j];
+                }
+
+                std::cout << dp[i][j] << '\t';
+            }
+            std::cout << std::endl;
+        }
+
+        return dp[s.size()][t.size()] == s.size();
+    }
+};
+
 int main()
 {
-    std::cout << "For Kirie" << std::endl;
+    Solution2 solution2;
+    if (solution2.isSubsequence("b", "c"))
+    {
+        std::cout << "True" << std::endl;
+    }
+    else
+    {
+        std::cout << "False" << std::endl;
+    }
     return 0;
 }
