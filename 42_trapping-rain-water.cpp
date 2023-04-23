a59b1b80184620fae244bfac423b3e74f94c28e9
Kirie <1048776386@qq.com>
2023-04-23 13:29:35 +0800

    42. 接雨水

---
+++ 42_trapping-rain-water.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+class Solution
+{
+public:
+    int trap(std::vector<int> &height)
+    {
+
+    }
+};
+
+int main()
+{
+    std::cout << "For Kirie" << std::endl;
+    return 0;
+}
