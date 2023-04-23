737a107e04fdcae9bb83506259fa00620d5b53d6
Kirie <1048776386@qq.com>
2023-04-23 13:21:19 +0800

    503. 下一个更大元素 II

--- 503_next-greater-element-ii.cpp
+++ 503_next-greater-element-ii.cpp
@@ -7,8 +7,14 @@ class Solution
 public:
     std::vector<int> nextGreaterElements(std::vector<int> &nums)
     {
+        if (nums.size() == 1)
+        {
+            return {-1};
+        }
+
         int numsSize = nums.size();
         std::vector<int> ans(numsSize, -1);
+        std::vector<bool> used(numsSize, false);
         std::stack<int> stack;
         stack.push(0);
         for (int i = 1; !stack.empty() && stack.top() != i; i = (i + 1) % numsSize)
@@ -17,9 +23,35 @@ public:
             {
                 int topIndex = stack.top();
                 stack.pop();
-                ans[topIndex] = i;
+                ans[topIndex] = nums[i];
             }
-            stack.push(i);
+            if (!used[i])
+            {
+                stack.push(i);
+                used[i] = true;
+            }
+        }
+        return ans;
+    }
+};
+
+class Solution2
+{
+public:
+    std::vector<int> nextGreaterElements(std::vector<int> &nums)
+    {
+        int size = nums.size();
+        std::vector<int> ans(size, -1);
+        std::stack<int> stack;
+        for (int i = 0; i < 2 * size; ++i)
+        {
+            int index = i % size;
+            while (!stack.empty() && nums[stack.top()] < nums[index])
+            {
+                ans[stack.top()] = nums[index];
+                stack.pop();
+            }
+            stack.push(index);
         }
         return ans;
     }
