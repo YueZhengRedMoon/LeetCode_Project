b6a6ea1f097bb5887baf8da675f0621359887e50
For Kirie <1048776386@qq.com>
2023-04-28 10:18:48 +0800

    1172. 餐盘栈

--- 1172_dinner-plate-stacks.cpp
+++ 1172_dinner-plate-stacks.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <stack>
 #include <vector>
+#include <set>
 
 /** 超时 */
-class DinnerPlates
+class MyDinnerPlates
 {
 public:
-    DinnerPlates(int cap) : capacity(cap), firstEmptyStackIndex(0), lastNotEmptyStackIndex(-1)
+    MyDinnerPlates(int cap) : capacity(cap), firstEmptyStackIndex(0), lastNotEmptyStackIndex(-1)
     {
         stacks.reserve(100001);
         stacks.emplace_back(std::stack<int>());
@@ -74,6 +75,98 @@ private:
     int capacity;
     int firstEmptyStackIndex;
     int lastNotEmptyStackIndex;
+};
+
+class DinnerPlates
+{
+public:
+    DinnerPlates(int c) : capacity(c)
+    {
+        stack.reserve(100001);
+        top.reserve(100001);
+    }
+
+    void push(int val)
+    {
+        if (poppedPos.empty())
+        {
+            int pos = stack.size();
+            stack.emplace_back(val);
+            if (pos % capacity == 0)
+            {
+                top.emplace_back(0);
+            }
+            else
+            {
+                ++top.back();
+            }
+        }
+        else
+        {
+            int pos = *poppedPos.begin();
+            poppedPos.erase(pos);
+            stack[pos] = val;
+            int index = pos / capacity;
+            ++top[index];
+        }
+    }
+
+    int pop()
+    {
+        while (!stack.empty() && poppedPos.find(stack.size() - 1) != poppedPos.end())
+        {
+            stack.pop_back();
+            int pos = *poppedPos.rbegin();
+            poppedPos.erase(pos);
+            if (pos % capacity == 0)
+            {
+                top.pop_back();
+            }
+        }
+
+        if (stack.empty())
+        {
+            return - 1;
+        }
+        else
+        {
+            int pos = stack.size() - 1;
+            int val = stack.back();
+            stack.pop_back();
+            if (pos % capacity == 0)
+            {
+                top.pop_back();
+            }
+            else
+            {
+                --top.back();
+            }
+            return val;
+        }
+    }
+
+    int popAtStack(int index)
+    {
+        if (index >= top.size())
+        {
+            return -1;
+        }
+        int stackTop = top[index];
+        if (stackTop < 0)
+        {
+            return -1;
+        }
+        --top[index];
+        int pos = index * capacity + stackTop;
+        poppedPos.emplace(pos);
+        return stack[pos];
+    }
+
+private:
+    int capacity;
+    std::vector<int> stack;
+    std::vector<int> top;
+    std::set<int> poppedPos;
 };
 
 int main()
