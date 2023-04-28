033e61146d46e4d12ea468d6d0ecf724ae56da37
For Kirie <1048776386@qq.com>
2023-04-28 22:02:17 +0800

    2602. 使数组元素全部相等的最少操作次数

---
+++ 2602_minimum-operations-to-make-all-array-elements-equal.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include "debug.h"
+
+class Solution
+{
+public:
+    std::vector<long long> minOperations(std::vector<int> &nums, std::vector<int> &queries)
+    {
+        int numsSize = nums.size();
+        std::sort(nums.begin(), nums.end());
+        std::vector<long long> prefix(numsSize, 0);
+        prefix[0] = nums[0];
+        for (int i = 1; i < numsSize; ++i)
+        {
+            prefix[i] = nums[i] + prefix[i - 1];
+        }
+
+        std::vector<long long> result(queries.size());
+        for (int i = 0; i < queries.size(); ++i)
+        {
+            long long query = queries[i];
+            auto it = std::upper_bound(nums.begin(), nums.end(), queries[i]);
+            int index = it - nums.begin();
+            if (index < nums.size())
+            {
+                if (index > 0)
+                {
+                    result[i] = prefix[numsSize - 1] - prefix[index - 1] - (numsSize - index) * query
+                                + index * query - prefix[index - 1];
+                }
+                else
+                {
+                    result[i] = prefix[numsSize - 1] - numsSize * query;
+                }
+            }
+            else
+            {
+                result[i] = numsSize * query - prefix[numsSize - 1];
+            }
+        }
+
+        return result;
+    }
+};
+
+int main()
+{
+    std::vector<int> nums = {3, 1, 6, 8};
+    std::vector<int> queries = {1, 5};
+    Solution solution;
+    std::vector<long long> result = solution.minOperations(nums, queries);
+    debug::printVector(result);
+    return 0;
+}
