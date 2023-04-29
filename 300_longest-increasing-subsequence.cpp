42955a9d8319af77ad805f153de7c3cd314cccdd
For Kirie <1048776386@qq.com>
2023-04-29 21:24:18 +0800

    300. 最长递增子序列

--- 300_longest-increasing-subsequence.cpp
+++ 300_longest-increasing-subsequence.cpp
@@ -29,6 +29,54 @@ public:
     }
 };
 
+/** 贪心 + 二分查找
+ *  要使上升子序列尽可能的长，需要让序列上升的尽可能慢，因此希望每次在上升子序列最后加上的那个数尽可能小
+ *  时间复杂度O(nlogn) */
+class Solution2
+{
+public:
+    int lengthOfLIS(std::vector<int> &nums)
+    {
+        int len = 1;    // 当前子序列的长度
+        int size = nums.size();
+        // dp[i]:长度为i的最长上升子序列的末尾元素的最小值
+        std::vector<int> dp(size + 1, 0);
+        dp[1] = nums[0];
+
+        for (int i = 1; i < size; ++i)
+        {
+            if (nums[i] > dp[len])
+            {
+                dp[++len] = nums[i];
+            }
+            // nums[i] <= dp[len]
+            else
+            {
+                // 在dp数组中二分查找，找到第一个比nums[i]小的数dp[k]，并更新dp[k+1]=nums[i]
+                int left = 1, right = len;
+                int pos = 0;    // 如果所有数都比nums[i]大，即此时nums[i]是最小的，则要更新长度为1的最长上升子序列的末尾元素的最小值
+                while (left <= right)
+                {
+                    int mid = (left + right) >> 1;
+                    if (dp[mid] < nums[i])
+                    {
+                        pos = mid;  // 记录比nums[i]小的数的下标
+                        left = mid + 1;
+                    }
+                    // dp[mid] >= nums[i]
+                    else
+                    {
+                        right = mid - 1;
+                    }
+                }
+                dp[pos + 1] = nums[i];
+            }
+        }
+
+        return len;
+    }
+};
+
 int main()
 {
     std::cout << "For Kirie!" << std::endl;
