54e1eeae7f2b78fe999b325606f53faa2100e6dc
For Kirie <1048776386@qq.com>
2023-05-04 22:20:55 +0800

    kirie_algorithm

---
+++ kirie_algorithm.h
@@ -0,0 +1,69 @@
+//
+// Created by Kirie on 2023/5/4.
+//
+
+#ifndef __LEET_CODE_PROJECT_ALGORITHM_H
+#define __LEET_CODE_PROJECT_ALGORITHM_H
+#include <functional>
+#include <iostream>
+
+namespace kirie
+{
+    /** 在区间[0, n)根据可调用对象comp找到第一个大于或大于等于target的数的索引，传入comp的参数是区间的中点mid，
+     *  comp应该具有如下形式：
+     *  return nums[mid] > target;  // 在nums[0:n)中找到第一个(最小的)大于target的数，返回其索引
+     *  return nums[mid] >= target; // 在nums[0:n)中找到第一个(最小的)大于等于target的数，返回其索引
+     *  */
+    int minGreaterEqual(int n, std::function<bool(int)> comp)
+    {
+        int left = 0, right = n;
+        while (left < right)
+        {
+            int mid = left + (right - left) / 2;
+            // nums[mid] >= target
+            // nums[mid] > target
+            if (comp(mid))
+            {
+                right = mid;
+            }
+            // nums[mid] < target
+            // nums[mid] <= target
+            else
+            {
+                // nums[mid] < target --> nums[mid + 1]可能>=target
+                // nums[mid] <= target --> nums[mid + 1]可能>target
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    /** 在区间[0, n)根据可调用对象comp找到最后一个小于或小于等于target的数的索引，传入comp的参数是区间的中点mid，
+     *  comp应该具有如下形式：
+     *  return nums[mid] < target;  // 在nums[0:n)中找到第一个(最大的)小于target的数，返回其索引
+     *  return nums[mid] <= target; // 在nums[0:n)中找到第一个(最大的)小于等于target的数，返回其索引
+     *  */
+    int maxLessEqual(int n, std::function<bool(int)> comp)
+    {
+        int left = 0, right = n;
+        while (left < right)
+        {
+            int mid = left + (right - left) / 2;
+            // nums[mid] <= target
+            // nums[mid] < target
+            if (comp(mid))
+            {
+                left = mid + 1;
+            }
+            // nums[mid] > target
+            // nums[mid] >= target
+            else
+            {
+                right = mid;
+            }
+        }
+        return left - 1;
+    }
+}
+
+#endif //__LEET_CODE_PROJECT_ALGORITHM_H
--- main.cpp
+++ main.cpp
@@ -2,100 +2,18 @@
 #include <algorithm>
 #include <vector>
 #include <queue>
-
-struct Person
-{
-    std::string name;
-    int age;
-};
-
-class PersonAgeLess
-{
-public:
-    // 大顶堆优先队列比较函数
-    bool operator()(const Person &lhs, const Person &rhs) const
-    {
-        return lhs.age < rhs.age;
-    }
-};
-
-class PersonAgeGreater
-{
-public:
-    // 小顶堆优先队列比较函数
-    bool operator()(const Person &lhs, const Person &rhs) const
-    {
-        return lhs.age > rhs.age;
-    }
-};
-
-void printVector(std::vector<int>& nums)
-{
-    for (int i = 0; i < nums.size(); ++i)
-    {
-        std::cout << nums[i] << ' ';
-    }
-    std::cout << std::endl;
-}
+#include "kirie_algorithm.h"
 
 int main()
 {
-    std::vector<Person> persons = {{"Alice", 19}, {"Dorothy", 18}, {"Lili", 28}};
-    std::make_heap(persons.begin(), persons.end(), PersonAgeLess());
-    Person kirie = {"Kirie", 300};
-    persons.push_back(kirie);
-    std::push_heap(persons.begin(), persons.end(), PersonAgeLess());
-    std::cout << "加入堆后" << std::endl;
-    for (const auto& person : persons)
-    {
-        std::cout << person.name << "(" << person.age << ") ";
-    }
-    std::cout << std::endl;
-
-    std::pop_heap(persons.begin(), persons.end(), PersonAgeLess());
-    Person top = persons.back();
-    std::cout << "top.name = " << top.name << ", top.age = " << top.age << std::endl;
-    std::cout << "弹出堆后" << std::endl;
-    for (const auto& person : persons)
-    {
-        std::cout << person.name << "(" << person.age << ") ";
-    }
-    std::cout << std::endl;
-
-    std::vector<int> vec{3, 2, 1, 5, 6, 4};
-    std::make_heap(vec.begin(), vec.end(), std::less<int>());   // 大顶堆
-
-    std::cout << "大顶堆的元素：";
-    printVector(vec);
-
-    std::cout << "小顶堆的元素：";
-    std::make_heap(vec.begin(), vec.end(), std::greater<int>());    // 小顶堆
-    printVector(vec);
-
-    std::priority_queue<Person, std::vector<Person>, PersonAgeGreater> priorityQueue;
-    priorityQueue.push({"Alice", 19});
-    priorityQueue.push({"Dorothy", 18});
-    priorityQueue.push({"Lili", 28});
-
-    std::cout << "优先队列中的元素：\n";
-    while (!priorityQueue.empty())
-    {
-        std::cout << priorityQueue.top().name << " (" << priorityQueue.top().age << ")\n";
-        priorityQueue.pop();
-    }
-    std::priority_queue<int, std::vector<int>, std::greater<int> > intPriorityQueue;
-    intPriorityQueue.push(5);
-    intPriorityQueue.push(4);
-    intPriorityQueue.push(1);
-    intPriorityQueue.push(3);
-    intPriorityQueue.push(2);
-    std::vector<int> intVec;
-    while (!intPriorityQueue.empty())
-    {
-        intVec.push_back(intPriorityQueue.top());
-        intPriorityQueue.pop();
-    }
-    std::cout << "内置类型优先队列指定std::greater为排序函数排序结果：";
-    printVector(intVec);
+    std::vector<int> vec = {1, 3, 4, 6, 9, 14, 16, 27, 30, 32, 35};
+    int maxIndex = kirie::maxLessEqual(vec.size(), [&](int mid) {
+        return vec[mid] < 30;
+    });
+    std::cout << "maxIndex = " << maxIndex << ", vec[maxIndex] = " << vec[maxIndex] << std::endl;
+    int minIndex = kirie::minGreaterEqual(vec.size(), [&](int mid){
+        return vec[mid] >= 16;
+    });
+    std::cout << "minIndex = " << minIndex << ", vec[minIndex] = " << vec[minIndex] << std::endl;
     return 0;
 }
