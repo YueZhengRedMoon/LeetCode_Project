0a48a5d360d4be1ef81ff029b35a613bb88976ab
Kirie <1048776386@qq.com>
2023-05-10 02:07:29 +0800

    380. O(1) 时间插入、删除和获取随机元素

---
+++ 380_insert-delete-getrandom-o1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <unordered_set>
+
+class RandomizedSet
+{
+public:
+    RandomizedSet()
+    {
+
+    }
+
+    bool insert(int val)
+    {
+
+    }
+
+    bool remove(int val)
+    {
+
+    }
+
+    int getRandom()
+    {
+
+    }
+
+private:
+    std::unordered_set<int> randomizedSet;
+};
+
+
+int main()
+{
+    std::cout << "For Kirie" << std::endl;
+    return 0;
+}
