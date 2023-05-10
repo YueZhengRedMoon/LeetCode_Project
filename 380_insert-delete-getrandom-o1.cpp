4f1e43944e063d84e926c4a98e9426f6b3089fe6
For Kirie <1048776386@qq.com>
2023-05-10 10:35:32 +0800

    380. O(1) 时间插入、删除和获取随机元素

--- 380_insert-delete-getrandom-o1.cpp
+++ 380_insert-delete-getrandom-o1.cpp
@@ -1,31 +1,58 @@
 #include <iostream>
-#include <unordered_set>
+#include <unordered_map>
+#include <random>
+#include <time.h>
 
 class RandomizedSet
 {
 public:
     RandomizedSet()
     {
-
+        randomEngine.seed(time(0));
     }
 
     bool insert(int val)
     {
-
+        if (indices.find(val) == indices.end())
+        {
+            indices[val] = nums.size();
+            nums.push_back(val);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
     bool remove(int val)
     {
-
+        if (indices.find(val) == indices.end())
+        {
+            return false;
+        }
+        else
+        {
+            int valIndex = indices[val];
+            indices[nums.back()] = valIndex;
+            std::swap(nums[valIndex], nums.back());
+            nums.erase(nums.end() - 1);
+            indices.erase(val);
+            return true;
+        }
     }
 
     int getRandom()
     {
-
+        std::uniform_int_distribution<unsigned int> distribution(0, nums.size() - 1);
+        unsigned int randomIndex = distribution(randomEngine);
+        return nums[randomIndex];
     }
 
 private:
-    std::unordered_set<int> randomizedSet;
+    std::vector<int> nums;
+    std::unordered_map<int, int> indices;
+    std::default_random_engine randomEngine;
 };
 
 
