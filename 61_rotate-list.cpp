e5c8b99390ddd8a2f80d4c398f5d44314b750e91
For Kirie <1048776386@qq.com>
2023-05-26 22:52:01 +0800

    61. 旋转链表

---
+++ 61_rotate-list.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include "leetcode.h"
+
+class Solution
+{
+public:
+    ListNode* rotateRight(ListNode* head, int k)
+    {
+        if (head == nullptr) return nullptr;
+        ListNode *fast = head;
+        int len = 0;
+        while (fast)
+        {
+            ++len;
+            fast = fast->next;
+        }
+        k %= len;
+        ListNode *slow = head;
+        for (int i = 1; i < len - k; ++i)
+        {
+            slow = slow->next;
+        }
+        std::cout << slow->val << std::endl;
+        // 先反转从头节点到slow，再反转slow->next到链表尾
+        ListNode *head2 = slow->next;
+        ListNode *head1 = reverse(head, slow->next);
+        head2 = reverse(head2, nullptr);
+        head->next = head2; // 原链表头head现在变成了head1的链表尾
+        ListNode *newHead = reverse(head1, nullptr);
+        return newHead;
+    }
+
+private:
+    ListNode *reverse(ListNode *head, ListNode *tail)
+    {
+        ListNode *prev = nullptr, *p = head;
+        while (p != tail)
+        {
+            ListNode *pNext = p->next;
+            p->next = prev;
+            prev = p;
+            p = pNext;
+        }
+        return prev;
+    }
+};
+
+class Solution2
+{
+public:
+    ListNode* rotateRight(ListNode* head, int k)
+    {
+        if (head == nullptr) return nullptr;
+        int len = 0;
+        ListNode *p = head, *prev = nullptr;
+        while (p)
+        {
+            ++len;
+            prev = p;
+            p = p->next;
+        }
+        prev->next = head;
+        p = head;
+        k %= len;
+        for (int i = 0; i < len - k; ++i)
+        {
+            prev = p;
+            p = p->next;
+        }
+        prev->next = nullptr;
+        return p;
+    }
+};
+
+int main()
+{
+    Solution2 solution;
+    ListNode *head = debug::createLinkedList({1, 2, 3, 4, 5});
+    ListNode *newHead = solution.rotateRight(head, 2);
+    debug::printLinkedList(newHead);
+    debug::freeLinkedList(newHead);
+    return 0;
+}
--- leetcode.h
+++ leetcode.h
@@ -4,6 +4,8 @@
 
 #ifndef __LEET_CODE_PROJECT_LEETCODE_H
 #define __LEET_CODE_PROJECT_LEETCODE_H
+#include <iostream>
+
 struct ListNode
 {
     int val;
@@ -22,4 +24,40 @@ struct TreeNode
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
+
+namespace debug
+{
+    ListNode* createLinkedList(const std::initializer_list<int> &list)
+    {
+        ListNode sentinel;
+        ListNode *p = &sentinel;
+        for (int val : list)
+        {
+            p->next = new ListNode(val);
+            p = p->next;
+        }
+        return sentinel.next;
+    }
+
+    void freeLinkedList(ListNode *head)
+    {
+        while (head)
+        {
+            ListNode *toDel = head;
+            head = head->next;
+            delete toDel;
+        }
+    }
+
+    void printLinkedList(ListNode *head)
+    {
+        ListNode *p = head;
+        while (p)
+        {
+            std::cout << p->val << ' ';
+            p = p->next;
+        }
+        std::cout << std::endl;
+    }
+}
 #endif //__LEET_CODE_PROJECT_LEETCODE_H
