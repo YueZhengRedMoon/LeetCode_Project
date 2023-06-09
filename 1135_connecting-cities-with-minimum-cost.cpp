9557164737046e8a7c506fb5c3636232531e7065
Kirie <1048776386@qq.com>
2023-06-10 01:57:44 +0800

    1135. 最低成本联通所有城市（Kruskal算法）

--- 1135_connecting-cities-with-minimum-cost.cpp
+++ 1135_connecting-cities-with-minimum-cost.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <forward_list>
 #include <queue>
+#include <algorithm>
+#include <numeric>
 
 class Solution
 {
@@ -55,8 +57,73 @@ public:
     }
 };
 
+class Solution2
+{
+public:
+    int minimumCost(int n, std::vector<std::vector<int>> &connections)
+    {
+        parent.resize(n);
+        std::iota(parent.begin(), parent.end(), 0);
+        rank.resize(n, 0);
+
+        std::sort(connections.begin(), connections.end(),
+                  [](const std::vector<int> &lhs, const std::vector<int> &rhs) -> bool {
+                      return lhs[2] < rhs[2];
+        });
+
+        int ans = 0;
+        for (const auto &e : connections)
+        {
+            if (unite(e[0] - 1, e[1] - 1))
+            {
+                ans += e[2];
+            }
+        }
+
+        int root = find(0);
+        for (int i = 1; i < n; ++i)
+        {
+            if (find(i) != root)
+                return -1;
+        }
+        return ans;
+    }
+
+private:
+    std::vector<int> parent;
+    std::vector<int> rank;
+
+    int find(int x)
+    {
+        if (x == parent[x]) return x;
+        else return parent[x] = find(parent[x]);
+    }
+
+    /** 返回true表示合并成功，返回false表示x,y已经在同一个集合中，合并失败 */
+    bool unite(int x, int y)
+    {
+        x = find(x);
+        y = find(y);
+        if (x == y) return false;
+        if (rank[x] < rank[y])
+        {
+            parent[x] = y;
+        }
+        else
+        {
+            parent[y] = x;
+            if (rank[x] == rank[y])
+                ++rank[x];
+        }
+        return true;
+    }
+};
+
 int main()
 {
-    std::cout << "For Kirie" << std::endl;
+    Solution solution;
+    std::vector<std::vector<int>> connections = {{1,2,5},{1,3,6},{2,3,1}};
+    int ans = solution.minimumCost(3, connections);
+    std::cout << ans << std::endl;
     return 0;
 }
