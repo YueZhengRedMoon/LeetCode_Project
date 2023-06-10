c664fa52d58fb0a44df2ac2f6fc8bd859ab8b4b3
For Kirie <1048776386@qq.com>
2023-06-10 15:55:27 +0800

    265. 粉刷房子 II

--- 265_paint-house-ii.cpp
+++ 265_paint-house-ii.cpp
@@ -11,14 +11,14 @@ public:
         const int inf = std::numeric_limits<int>::max();
         int n = costs.size(), k = costs[0].size();
         // dp[i][j]:将i号房子染成j色的最小成本
-        std::vector<std::vector<int>> dp(2, std::vector<int>(k));
+        // std::vector<int> dp(k);
         // 将房子染色需要的最小成本以及最后一个房子被染成的颜色
         int min1 = inf, color1 = 0;
         // 将房子染色需要的第二小成本
         int min2 = inf;
         for (int i = 0; i < k; ++i)
         {
-            dp[0][i] = costs[0][i];
+            // dp[i] = costs[0][i];
             if (costs[0][i] < min1)
             {
                 min2 = min1;
@@ -33,29 +33,31 @@ public:
 
         for (int i = 1; i < n; ++i)
         {
-            int cur = i % 2;
             int lastMin1 = min1, lastColor1 = color1;
             int lastMin2 = min2;
+            int cost;
             min1 = min2 = inf;
             for (int j = 0; j < k; ++j)
             {
                 if (j != lastColor1)
                 {
-                    dp[cur][j] = lastMin1 + costs[i][j];
+                    // dp[j] = lastMin1 + costs[i][j];
+                    cost = lastMin1 + costs[i][j];
                 }
                 else
                 {
-                    dp[cur][j] = lastMin2 + costs[i][j];
+                    // dp[j] = lastMin2 + costs[i][j];
+                    cost = lastMin2 + costs[i][j];
                 }
-                if (dp[cur][j] < min1)
+                if (cost < min1)
                 {
                     min2 = min1;
-                    min1 = dp[cur][j];
+                    min1 = cost;
                     color1 = j;
                 }
-                else if (dp[cur][j] < min2)
+                else if (cost < min2)
                 {
-                    min2 = dp[cur][j];
+                    min2 = cost;
                 }
             }
         }
