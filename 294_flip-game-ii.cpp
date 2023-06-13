bf23eb6faa0dce53edb197590d36c7957a7fa757
For Kirie <1048776386@qq.com>
2023-06-13 12:42:07 +0800

    294. 翻转游戏 II

--- 294_flip-game-ii.cpp
+++ 294_flip-game-ii.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
+#include <algorithm>
 
+/** 错误 */
 class Solution
 {
 public:
@@ -42,8 +45,117 @@ public:
     }
 };
 
+/** 记忆化搜索 */
+class Solution2
+{
+public:
+    bool canWin(std::string state)
+    {
+        return backtracking(state);
+    }
+
+private:
+    std::unordered_map<std::string, bool> memo;
+
+    bool backtracking(std::string &state)
+    {
+        auto it = memo.find(state);
+        if (it != memo.end())
+            return it->second;
+
+        int n = state.size();
+        for (int i = 1; i < n; ++i)
+        {
+            if (state[i - 1] == '+' && state[i] == '+')
+            {
+                state[i - 1] = state[i] = '-';
+                bool res = backtracking(state);
+                state[i - 1] = state[i] = '+';
+                if (!res)
+                    return memo[state] = true;
+            }
+        }
+        return memo[state] = false;
+    }
+};
+
+class Solution3
+{
+public:
+    bool canWin(std::string state)
+    {
+        // 记录当前输入字符串的所有子状态，一个子状态是字符串中一段连续'+'的长度
+        std::vector<int> subStates;
+        int len = state.size(), i = 0, j = 0;
+        int max = -1;   // 最长的子状态
+        while (i < len)
+        {
+            if (state[i] == '-')
+            {
+                ++i;
+            }
+            else
+            {
+                // 遇到了'+'，看看连续的'+'最多有多长。
+                for (j = i; j < len && state[j] == '+'; ++j);
+                int subState = j - i;
+                subStates.push_back(subState);
+                max = std::max(max, subState);
+                i = j;
+            }
+        }
+        // 输入字符串中不存在长度>=2的连续'+'子串，先手必输
+        if (max <= 1)
+            return false;
+
+        // 通过动态规划计算SG函数的g值
+        // Sprague-Grundy定理：
+        // 如果一个节点x的g(x)=0，那么x状态一定是“先手必输”的状态，否则一定是“先手必赢”
+        // 如果x的子状态是{x1, x2, ……, xn}SG函数g(x)=g(x1)^(x2)^……^g(xn)
+        std::vector<int> dp(max + 1, 0);
+        std::vector<int> temp;
+        dp[2] = 1;
+
+        // 计算g[i]的值，获取下一步的所有状态的g值
+        for (i = 3; i <= max; ++i)
+        {
+            // 把s[j]和s[j+1]从++变成--，连续的+变成两段，长度分别为j和i-j-2
+            for (j = 0; j < i / 2; ++j)
+            {
+                temp.push_back(dp[j] ^ dp[i - j - 2]);
+            }
+
+            // 找到第一个缺失的非负整数
+            std::sort(temp.begin(), temp.end());
+            int mis = 0;
+            for (int k = 0; k < temp.size(); ++k)
+            {
+                if (temp[k] > mis)
+                    break;
+                else if (temp[k] < mis)
+                    continue;
+                else
+                    ++mis;
+            }
+            dp[i] = mis;
+            temp.clear();
+        }
+
+        int ans = 0;
+        for (int subState : subStates)
+        {
+            ans ^= dp[subState];
+        }
+
+        return ans;
+    }
+};
+
 int main()
 {
-    std::cout << "For Kirie" << std::endl;
+    Solution2 solution;
+    std::string currentState = "++++";
+    bool ans = solution.canWin(currentState);
+    std::cout << std::boolalpha << ans << std::endl;
     return 0;
 }
