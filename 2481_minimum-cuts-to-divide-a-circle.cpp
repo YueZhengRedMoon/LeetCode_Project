8d2ec6ec7e89337414544375f4723fdfbebb3e5b
Kirie <1048776386@qq.com>
2023-06-17 00:39:29 +0800

    2481. 分割圆的最少切割次数

---
+++ 2481_minimum-cuts-to-divide-a-circle.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+class Solution
+{
+public:
+    int numberOfCuts(int n)
+    {
+        if (n == 1)
+            return 0;
+        if (n % 2 == 0)
+            return n / 2;
+        return n;
+    }
+};
+
+int main()
+{
+    std::cout << "For Kirie" << std::endl;
+    return 0;
+}
