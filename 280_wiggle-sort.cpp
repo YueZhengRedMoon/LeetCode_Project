078f9babab210886a9d8280e5bfd1c1e1f0cc5c6
Kirie <1048776386@qq.com>
2023-06-29 01:39:05 +0800

    280. 摆动排序

--- 280_wiggle-sort.cpp
+++ 280_wiggle-sort.cpp
@@ -6,8 +6,21 @@ class Solution
 public:
     void wiggleSort(std::vector<int> &nums)
     {
-        // 重新排序后的数组，偶数索引处的数比两边的数小，奇数索引处的数比两边的数大
-
+        // 重新排序后的数组，偶数索引的数比奇数索引的数小
+        int n = nums.size();
+        for (int i = 0, j = 1; j < n; i += 2, j += 2)
+        {
+            if (nums[i] > nums[j])
+            {
+                std::swap(nums[i], nums[j]);    // nums[i] <= nums[j]
+            }
+            if (j + 1 < n && nums[j + 1] > nums[j])   // j + 1是偶数索引
+            {
+                // nums[j+1] > nums[j] -> nums[j+1] > nums[i]
+                std::swap(nums[j], nums[j + 1]);
+            }
+            // nums[i] <= nums[j] >= nums[j+1],j为奇数
+        }
     }
 };
 
