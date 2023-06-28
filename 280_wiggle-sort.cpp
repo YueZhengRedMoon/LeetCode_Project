51a5b556d9fff7c2643766227b9870e9891abd78
For Kirie <1048776386@qq.com>
2023-06-28 23:30:24 +0800

    280. 摆动排序

---
+++ 280_wiggle-sort.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+class Solution
+{
+public:
+    void wiggleSort(std::vector<int> &nums)
+    {
+        // 重新排序后的数组，偶数索引处的数比两边的数小，奇数索引处的数比两边的数大
+
+    }
+};
+
+int main()
+{
+    std::cout << "For Kirie!" << std::endl;
+    return 0;
+}
