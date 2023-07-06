38e2a11345c4f121772a48d0695da4c24cebce22
For Kirie <1048776386@qq.com>
2023-07-06 23:16:08 +0800

    1429. 第一个唯一数字

---
+++ 1429_first-unique-number.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+class FirstUnique
+{
+public:
+    FirstUnique(std::vector<int> &nums)
+    {
+
+    }
+
+    int showFirstUnique()
+    {
+
+    }
+
+    void add(int value)
+    {
+
+    }
+};
+
+
+int main()
+{
+    std::cout << "For Kirie!" << std::endl;
+    return 0;
+}
