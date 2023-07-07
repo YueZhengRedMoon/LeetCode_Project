119fc09cf27974b0d46a575665e370930658b2f1
For Kirie <1048776386@qq.com>
2023-07-07 17:24:21 +0800

    1429. 第一个唯一数字

--- 1429_first-unique-number.cpp
+++ 1429_first-unique-number.cpp
@@ -1,23 +1,51 @@
 #include <iostream>
 #include <vector>
+#include <list>
+#include <unordered_map>
 
 class FirstUnique
 {
 public:
     FirstUnique(std::vector<int> &nums)
     {
-
+        for (int num : nums)
+        {
+            add(num);
+        }
     }
 
     int showFirstUnique()
     {
-
+        if (queue.empty())
+        {
+            return -1;
+        }
+        return queue.front();
     }
 
     void add(int value)
     {
-
+        auto mapIter = iterMap.find(value);
+        if (mapIter == iterMap.end())
+        {
+            queue.push_back(value);
+            std::list<int>::iterator listIter = queue.end();
+            --listIter;
+            iterMap[value] = std::make_pair(listIter, true);
+        }
+        else
+        {
+            if (mapIter->second.second)
+            {
+                queue.erase(mapIter->second.first);
+                mapIter->second.second = false;
+            }
+        }
     }
+
+private:
+    std::list<int> queue;
+    std::unordered_map<int, std::pair<std::list<int>::iterator, bool>> iterMap;
 };
 
 
