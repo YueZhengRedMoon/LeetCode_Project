fbb40b23ea67f45c5e9272f9c253c331f1dad682
For Kirie <1048776386@qq.com>
2023-07-11 17:14:40 +0800

    1214. 查找两棵二叉搜索树之和

--- 1214_two-sum-bsts.cpp
+++ 1214_two-sum-bsts.cpp
@@ -54,6 +54,76 @@ private:
     }
 };
 
+class Solution2
+{
+public:
+    bool twoSumBSTs(TreeNode *root1, TreeNode *root2, int target)
+    {
+        std::stack<TreeNode*> stack1, stack2;
+        TreeNode *cur1 = root1, *cur2 = root2;
+
+        // 遍历到root1中的最小节点
+        while (cur1->left)
+        {
+            stack1.push(cur1);
+            cur1 = cur1->left;
+        }
+        // 遍历到root2中的最大节点
+        while (cur2->right)
+        {
+            stack2.push(cur2);
+            cur2 = cur2->right;
+        }
+
+        while (cur1 && cur2)
+        {
+            int sum = cur1->val + cur2->val;
+            if (sum < target)   // 和小了，cur1增加
+            {
+                cur1 = cur1->right;
+                while (cur1 || !stack1.empty())
+                {
+                    if (cur1)
+                    {
+                        stack1.push(cur1);
+                        cur1 = cur1->left;
+                    }
+                    else
+                    {
+                        cur1 = stack1.top();
+                        stack1.pop();
+                        break;
+                    }
+                }
+            }
+            else if (sum > target)  // 和大了，cur2减小
+            {
+                cur2 = cur2->left;
+                while (cur2 || !stack2.empty())
+                {
+                    if (cur2)
+                    {
+                        stack2.push(cur2);
+                        cur2 = cur2->right;
+                    }
+                    else
+                    {
+                        cur2 = stack2.top();
+                        stack2.pop();
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
+
 int main()
 {
     std::cout << "For Kirie!" << std::endl;
