531ef4646a512c0bf8696a66ca39110272f1325c
Kirie <1048776386@qq.com>
2023-07-14 01:30:48 +0800

    979. 在二叉树中分配硬币

---
+++ 979_distribute-coins-in-binary-tree.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "leetcode.h"
+
+class Solution
+{
+public:
+    int distributeCoins(TreeNode* root)
+    {
+        ans = 0;
+        postorderTraversal(root);
+        return ans;
+    }
+
+private:
+    int ans;
+
+    int postorderTraversal(TreeNode *node)
+    {
+        if (node == nullptr)
+            return 0;
+
+        int left = postorderTraversal(node->left);
+        int right = postorderTraversal(node->right);
+        int cur = 1 - node->val;
+
+        int sum = left + cur + right;
+        ans += std::abs(sum);
+        return sum;
+    }
+};
+
+int main()
+{
+    std::cout << "For Kirie" << std::endl;
+    return 0;
+}
