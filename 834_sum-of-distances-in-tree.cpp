9ea175986844f921d69ed3390389998c07fd8d34
For Kirie <1048776386@qq.com>
2023-07-17 00:19:47 +0800

    834. 树中距离之和

---
+++ 834_sum-of-distances-in-tree.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+class Solution
+{
+public:
+    std::vector<int> sumOfDistancesInTree(int n, std::vector<std::vector<int>> &edges)
+    {
+
+    }
+};
+
+int main()
+{
+    std::cout << "For Kirie!" << std::endl;
+    return 0;
+}
