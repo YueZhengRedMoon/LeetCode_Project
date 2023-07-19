79bd8a757a6c0995c4c53b270657ab532c6571f0
For Kirie <1048776386@qq.com>
2023-07-19 10:35:49 +0800

    1197. 进击的骑士

--- 1197_minimum-knight-moves.cpp
+++ 1197_minimum-knight-moves.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <unordered_map>
+#include <unordered_set>
 #include <queue>
 
 class Solution
@@ -44,6 +45,61 @@ public:
         }
 
         return -1;
+    }
+
+private:
+    inline int hash(int x, int y)
+    {
+        return (x + 301) * 10000 + y;
+    }
+};
+
+class Solution2
+{
+public:
+    int minKnightMoves(int x, int y)
+    {
+        if (x == 0 && y == 0)
+            return 0;
+
+        // 4个象限关于（0，0）对称，都转化成第一象限
+        x = std::abs(x);
+        y = std::abs(y);
+
+        const int dir[8][2] = {{1, 2}, {2, 1}, {2, -1}, {1, -2},
+                               {-1, -2}, {-2, -1}, {-2, 1}, {-1, 2}};
+
+        // 记忆化BFS
+        std::queue<std::pair<int, int>> queue;
+        std::unordered_set<int> visited;
+        queue.emplace(0, 0);
+        visited.emplace(hash(0, 0));
+        int step = 0;
+        while (true)
+        {
+            int size = queue.size();
+            ++step;
+            while (size--)
+            {
+                auto [curX, curY] = queue.front();
+                queue.pop();
+                for (int i = 0; i < 8; ++i)
+                {
+                    int nx = curX + dir[i][0], ny = curY + dir[i][1];
+                    if (visited.find(hash(nx, ny)) == visited.end())
+                    {
+                        // 将搜索的范围限定在（0,0）和终点框成的正方形，之外再向外延伸2层的正方形区域中
+                        if (-5 <= nx && nx <= x + 5 && -5 <= ny && ny <= y + 5)
+                        {
+                            if (nx == x && ny == y)
+                                return step;
+                            queue.emplace(nx, ny);
+                            visited.emplace(hash(nx, ny));
+                        }
+                    }
+                }
+            }
+        }
     }
 
 private:
