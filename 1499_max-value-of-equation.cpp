f8d0137e11772e798471d807f5772f242758082f
For Kirie <1048776386@qq.com>
2023-07-21 18:57:26 +0800

    1499. 满足不等式的最大值

--- 1499_max-value-of-equation.cpp
+++ 1499_max-value-of-equation.cpp
@@ -29,6 +29,37 @@ public:
     }
 };
 
+class Solution2
+{
+public:
+    using Pair = std::pair<int, int>;
+
+    int findMaxValueOfEquation(std::vector<std::vector<int>> &points, int k)
+    {
+        // xj > xi -> yi + yj + |xi - xj| = xj + yj - xi + yi = xj + yj + yi - xi
+        int ans = std::numeric_limits<int>::min();
+        std::deque<Pair> deque;
+        for (const std::vector<int> &point : points)
+        {
+            int x = point[0], y = point[1];
+            while (!deque.empty() && x - deque.front().second > k)   // xj - xi > k
+            {
+                deque.pop_front();
+            }
+            if (!deque.empty())
+            {
+                ans = std::max(ans, x + y + deque.front().first);   // xj + yj + yi - xi
+            }
+            while (!deque.empty() && y - x >= deque.back().first)
+            {
+                deque.pop_back();
+            }
+            deque.emplace_back(y - x, x);   // yi - xi, xi
+        }
+        return ans;
+    }
+};
+
 int main()
 {
     std::cout << "For Kirie" << std::endl;
