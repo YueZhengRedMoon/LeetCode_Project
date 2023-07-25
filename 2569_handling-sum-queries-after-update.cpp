5fcc976e2e53f35aae2e9e4a7fcee2dcb60115c7
Kirie <1048776386@qq.com>
2023-07-26 01:17:42 +0800

    2569. 更新数组后处理求和查询

---
+++ 2569_handling-sum-queries-after-update.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+class Solution
+{
+public:
+    std::vector<long long> handleQuery(std::vector<int> &nums1, std::vector<int> &nums2, std::vector<std::vector<int>> &queries)
+    {
+
+    }
+};
+
+int main()
+{
+    std::cout << "For Kirie" << std::endl;
+    return 0;
+}
