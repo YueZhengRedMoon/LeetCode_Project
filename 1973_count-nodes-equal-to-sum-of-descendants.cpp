0eb1713793f0c43304dfbcdfa8324891a5837935
For Kirie <1048776386@qq.com>
2023-08-28 10:32:16 +0800

    1973. 值等于子节点值之和的节点数量

---
+++ 1973_count-nodes-equal-to-sum-of-descendants.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "leetcode.h"
+
+class Solution
+{
+public:
+    int equalToDescendants(TreeNode *root)
+    {
+        ans = 0;
+        postorderTraversal(root);
+        return ans;
+    }
+
+private:
+    int ans;
+
+    long long postorderTraversal(TreeNode *node)
+    {
+        if (node == nullptr)
+            return 0;
+
+        long long leftSubTreeSum = postorderTraversal(node->left);
+        long long rightSubTreeSum = postorderTraversal(node->right);
+        if (leftSubTreeSum + rightSubTreeSum == node->val)
+            ++ans;
+        return node->val + leftSubTreeSum + rightSubTreeSum;
+    }
+};
+
+int main()
+{
+    std::cout << "For Kirie!" << std::endl;
+    return 0;
+}
--- main.cpp
+++ main.cpp
@@ -5,44 +5,10 @@
 #include "kirie_algorithm.h"
 #include "debug.h"
 
-const int MOD = 1000000007;
-
-// 计算 (base ^ exp) % MOD 的结果
-long long fast_pow(long long base, long long exp) {
-    long long result = 1;
-    while (exp > 0) {
-        if (exp % 2 == 1) {
-            result = (result * base) % MOD;
-        }
-        base = (base * base) % MOD;
-        exp /= 2;
-    }
-    return result;
-}
-
-// 求逆元
-long long calc_inv(long long num) {
-    return fast_pow(num, MOD - 2); // 使用费马小定理计算 num^(MOD-2) % MOD
-}
-
-// 求组合数
-long long calc_combination(long long n, long long k) {
-    if (k == 0 || k == n) {
-        return 1;
-    }
-    long long result = 1;
-    for (long long i = 1; i <= k; ++i) {
-        result = (result * (n - i + 1)) % MOD;
-        result = (result * calc_inv(i)) % MOD;
-    }
-    return result;
-}
-
-
-//int array[] = {10, 20, 30};
-//std::cout << -2[array - 1];
 int main()
 {
-
+    int array[] = {10, 20, 30};
+    std::cout << 2[array] << std::endl;
+    std::cout << -2[array - 1] << std::endl;
     return 0;
 }
