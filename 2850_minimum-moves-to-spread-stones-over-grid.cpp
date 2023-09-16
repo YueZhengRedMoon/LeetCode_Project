e3bb8da245183c0e9ddbdf6d20f27c87a0d8b511
For Kirie <1048776386@qq.com>
2023-09-16 15:35:27 +0800

    2850. 将石头分散到网格图的最少移动次数

--- 2850_minimum-moves-to-spread-stones-over-grid.cpp
+++ 2850_minimum-moves-to-spread-stones-over-grid.cpp
@@ -1,7 +1,49 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
+// 暴力解法
 class Solution
+{
+public:
+    int minimumMoves(std::vector<std::vector<int>> &grid)
+    {
+        std::vector<std::pair<int, int>> from, to;
+        for (int i = 0; i < 3; ++i)
+        {
+            for (int j = 0; j < 3; ++j)
+            {
+                if (grid[i][j] > 1)
+                    from.insert(from.end(), grid[i][j] - 1, {i, j});
+                else if (grid[i][j] == 0)
+                    to.emplace_back(i, j);
+            }
+        }
+
+        int ans = 0x39C5BB;
+        do
+        {
+            int move = 0;
+            for (int i = 0; i < from.size(); ++i)
+            {
+                move += manhattan(from[i].first, from[i].second, to[i].first, to[i].second);
+            }
+            if (move < ans)
+                ans = move;
+        } while (std::next_permutation(from.begin(), from.end()));
+
+        return ans;
+    }
+
+private:
+    int manhattan(int x1, int y1, int x2, int y2)
+    {
+        return std::abs(x1 - x2) + std::abs(y1 - y2);
+    }
+};
+
+// 错误
+class Solution2
 {
 public:
     int minimumMoves(std::vector<std::vector<int>> &grid)
