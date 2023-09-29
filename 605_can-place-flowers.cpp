5024333520734fb04b27d336c7e34f138b8fc4f4
For Kirie <1048776386@qq.com>
2023-09-29 21:59:17 +0800

    605. 种花问题

---
+++ 605_can-place-flowers.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include "debug.h"
+
+class Solution
+{
+public:
+    bool canPlaceFlowers(std::vector<int> &flowerbed, int n)
+    {
+        int cnt = 0, size = flowerbed.size();
+        for (int i = 0; i < size && cnt < n; ++i)
+        {
+            if (flowerbed[i] == 0)
+            {
+                if ((i > 0 && flowerbed[i - 1] == 1) || (i + 1 < size && flowerbed[i + 1] == 1))
+                    continue;
+
+                flowerbed[i] = 1;
+                ++cnt;
+                logObj(i);
+            }
+        }
+        return cnt == n;
+    }
+};
+
+int main()
+{
+    Solution solution;
+    std::vector<int> flowerbed = {1,0,0,0,0,1};
+    int n = 2;
+    bool ans = solution.canPlaceFlowers(flowerbed, n);
+    std::cout << std::boolalpha << ans << std::endl;
+    return 0;
+}
