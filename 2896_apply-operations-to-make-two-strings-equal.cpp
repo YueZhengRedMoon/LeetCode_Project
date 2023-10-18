a5bfcf4f121cabf02830c7d0b30500d2ec0b2c45
For Kirie <1048776386@qq.com>
2023-10-18 16:48:48 +0800

    2896. 执行操作使两个字符串相等

---
+++ 2896_apply-operations-to-make-two-strings-equal.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+class Solution
+{
+public:
+    int minOperations(std::string s1, std::string s2, int x)
+    {
+        int n = s1.size();
+
+    }
+};
+
+int main()
+{
+    std::cout << "For Kirie!" << std::endl;
+    return 0;
+}
