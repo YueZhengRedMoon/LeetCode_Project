7da990454c50b6c09a19f65217c342f0245b999d
For Kirie <1048776386@qq.com>
2024-04-18 15:23:23 +0800

    2007. 从双倍数组中还原原数组

--- 2000-2499/2007_find-original-array-from-doubled-array.cpp
+++ 2000-2499/2007_find-original-array-from-doubled-array.cpp
@@ -49,6 +49,57 @@ public:
     }
 };
 
+class Solution2
+{
+public:
+    std::vector<int> findOriginalArray(std::vector<int>& changed)
+    {
+        int n = changed.size();
+        if (n % 2 == 1)
+            return {};
+
+        std::unordered_map<int, int> counter;
+        for (int x : changed)
+            ++counter[x];
+
+        // 单独处理0
+        int cnt0 = counter[0];
+        if (cnt0 % 2)
+            return {};
+        counter.erase(0);
+        std::vector<int> original(cnt0 / 2);
+        original.reserve(n / 2);
+
+        for (auto it = counter.begin(); it != counter.end(); ++it)
+        {
+            int x = it->first;
+            // 如果x/2在counter中，则跳过
+            if (x % 2 == 0 && counter.find(x / 2) != counter.end())
+                continue;
+
+            // 把x, 2x, 4x, 8x, ... 全部配对
+            while (counter.find(x) != counter.end())
+            {
+                int cntX = counter[x];
+                if (cntX > counter[x * 2])  // 至少需要cntX个2x与x配对
+                    return {};
+                original.insert(original.end(), cntX, x);
+                if (cntX < counter[x * 2])  // 还剩下一些2x
+                {
+                    counter[2 * x] -= cntX;
+                    x *= 2;
+                }
+                else    // x与2x匹配完了
+                {
+                    x *= 4;
+                }
+            }
+        }
+
+        return original;
+    }
+};
+
 int main()
 {
     std::cout << "For Kirie!" << std::endl;
