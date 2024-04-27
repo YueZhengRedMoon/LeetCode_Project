349ed36f7df1ccf1dd3a16650d8e7b62bb548160
For Kirie <1048776386@qq.com>
2024-04-27 11:30:14 +0800

    1157. 子数组中占绝大多数的元素（未完成）

---
+++ 1000-1499/1157_online-majority-element-in-subarray.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+class MajorityChecker
+{
+public:
+    MajorityChecker(std::vector<int>& arr)
+    {
+
+    }
+
+    int query(int left, int right, int threshold)
+    {
+
+    }
+
+private:
+    int n;
+    // 每个区间对应的出现次数最多的数以及对应的元素
+    std::vector<std::pair<int, std::vector<int>>> tree;
+};
+
+int main()
+{
+    std::cout << "For Kirie!" << std::endl;
+    return 0;
+}
