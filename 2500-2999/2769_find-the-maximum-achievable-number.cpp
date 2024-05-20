cc9cc63ba20b5999bc2aa8e8b68edf33ebb127e7
For Kirie <1048776386@qq.com>
2024-05-21 00:21:01 +0800

    2769. 找出最大的可达成数字

---
+++ 2500-2999/2769_find-the-maximum-achievable-number.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+class Solution
+{
+public:
+    int theMaximumAchievableX(int num, int t)
+    {
+        return num + t * 2;
+    }
+};
+
+int main()
+{
+    std::cout << "For Kirie!" << std::endl;
+    return 0;
+}
