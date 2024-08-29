a11b16c711a6ec2e4d0e97ed8e78497eaebc828e
For Kirie <1048776386@qq.com>
2024-08-29 22:50:00 +0800

    3144. 分割字符频率相等的最少子字符串（未做出）

---
+++ 3000-3499/3144_minimum-substring-partition-of-equal-character-frequency.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+class Solution
+{
+public:
+    int minimumSubstringsInPartition(std::string s)
+    {
+
+    }
+};
+
+int main()
+{
+    std::cout << "For Kirie!" << std::endl;
+    return 0;
+}
