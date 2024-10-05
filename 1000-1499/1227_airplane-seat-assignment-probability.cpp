7dc774089b75b30dc1cb66cab4f58a6f28660471
For Kirie <1048776386@qq.com>
2024-10-05 21:18:43 +0800

    1227. 飞机座位分配概率

---
+++ 1000-1499/1227_airplane-seat-assignment-probability.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+class Solution
+{
+public:
+    double nthPersonGetsNthSeat(int n)
+    {
+        return n == 1 ? 1.0 : 0.5;
+    }
+};
+
+int main()
+{
+    std::cout << "For Kirie!" << std::endl;
+    return 0;
+}
